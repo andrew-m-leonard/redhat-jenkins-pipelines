04546149eb8ef8c24fbc5fafc2b7e28f80f90997
Andrew Leonard <anleonar@redhat.com>
2023-06-02 11:40:31 +0100

    Setup pipeline job config

--- pipelines/jobs/configurations/jdk11u.groovy
+++ pipelines/jobs/configurations/jdk11u.groovy
@@ -1,6 +1,11 @@
 targetConfigurations = [
 ]
 
+// 18:05 Tue, Thur
+triggerSchedule_nightly = 'TZ=UTC\n05 18 * * 2,4'
+// 17:05 Sat
+triggerSchedule_weekly = 'TZ=UTC\n05 17 * * 6'
+
 // scmReferences to use for weekly release build
 weekly_release_scmReferences = [
 ]
